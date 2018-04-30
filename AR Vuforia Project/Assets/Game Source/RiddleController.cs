252538e5c529b29957bc593141b2caf1690cf68f
Luiz Felipe Escandiuzzi <37418595+Escandiuzzi@users.noreply.github.com>
2018-04-30 10:27:00 -0300

    Riddle

---
+++ AR Vuforia Project/Assets/Game Source/RiddleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RiddleController : MonoBehaviour {
+
+    public InputField Field;
+    public string answer;
+    public FloatingText fText;
+    public FloatingText wText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    public void CheckAnswer()
+    {
+   
+        if (Field.text == answer)
+        {
+            fText.PopUpText();
+        }
+        else
+        {
+            wText.PopUpText();
+        }
+     
+    }
+}
